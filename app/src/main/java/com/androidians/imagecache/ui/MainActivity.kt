9e572c17226c0ae75c39e136645038a77e3c999b
nithin-aktivolabs <nithin@aktivolabs.com>
2021-12-22 23:05:41 +0530

    get random imageUrl and downloading the image using downloadManager to context external directory

--- app/src/main/java/com/androidians/imagecache/ui/MainActivity.kt
+++ app/src/main/java/com/androidians/imagecache/ui/MainActivity.kt
@@ -1,18 +1,36 @@
 package com.androidians.imagecache.ui
 
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.androidians.imagecache.data.local.LocalStorage
 import com.androidians.imagecache.databinding.ActivityMainBinding
+import java.io.File
 
+const val IMAGE_CACHE_FILE_NAME = "image_cache.jpg"
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val downloadManager: DownloadManager by lazy {
+        getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+    }
     private val viewModel : MainViewModel by viewModels()
     private var randomImageUrl: String = ""
+    private val onCompleteReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            binding.getImageBtn.isEnabled = true
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +38,17 @@ class MainActivity : AppCompatActivity() {
         setObservers()
         getUrlList()
         init()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        LocalBroadcastManager.getInstance(this).registerReceiver(onCompleteReceiver,
+            IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(onCompleteReceiver)
     }
 
     // observers to get the random url
@@ -33,8 +62,21 @@ class MainActivity : AppCompatActivity() {
     // to initialize
     private fun init() {
         binding.getImageBtn.setOnClickListener {
-            viewModel.getImageFromUrl(randomImageUrl)
+            downloadImage(randomImageUrl)
         }
+    }
+
+    private fun downloadImage(randomImageUrl: String) {
+        val file = File(getExternalFilesDir(null), IMAGE_CACHE_FILE_NAME)
+        val downloadUri: Uri = Uri.parse(randomImageUrl)
+        val request = DownloadManager.Request(downloadUri)
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+            .setAllowedOverRoaming(false)
+            .setTitle(randomImageUrl)
+            .setMimeType("image/*")
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setDestinationUri(file.toUri())
+        downloadManager.enqueue(request)
     }
 
     // first try to get the url list from cache, else get it from assets
